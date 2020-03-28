c9fc69a0c32e9a19719cdd59cdab705be18c6fc0
ConorTapley <34166407+ConorTapley@users.noreply.github.com>
2020-03-28 08:49:19 -0800

    ball movement

---
+++ Chrome Ball/Assets/PlayerMovement.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    private Rigidbody rb;
+    [SerializeField] float speed = 3f;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        speed *= 10;
+    }
+    
+    void Update()
+    {
+        float horizontalMovement = Input.GetAxis("Horizontal");
+        float verticalMovement = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(horizontalMovement, 0.0f, verticalMovement);
+
+        rb.AddForce(movement * speed * Time.deltaTime);
+    }
+}
